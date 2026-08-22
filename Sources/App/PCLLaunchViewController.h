e1b9d2b10761e96787f4ea68e57e6aa6711a3370
Robit-space <207705032+Robit-space@users.noreply.github.com>
2026-08-22 13:42:46 +0800

    Fix CE launch interface and CI diagnostics

--- Sources/App/PCLLaunchViewController.h
+++ Sources/App/PCLLaunchViewController.h
@@ -5,4 +5,10 @@
 @property (nonatomic, assign) CGFloat leftPanelWidth;
 @property (nonatomic, copy) void (^onOpenDownload)(void);
 
+
+- (void)dismissTransientUI;
+- (void)prepareCEEnterAnimation;
+- (void)playCEEnterAnimation;
+- (void)playCEExitWithCompletion:(dispatch_block_t)completion;
+
 @end
